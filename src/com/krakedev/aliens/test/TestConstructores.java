d7f5176fdba95b2b06428a931d63709d1c231a5a
JonathanLuna95 <jjlunap95@gmail.com>
2026-08-16 13:55:50 -0500

    Agregar pruebas de constructores de Alien

---
+++ src/com/krakedev/aliens/test/TestConstructores.java
@@ -0,0 +1,22 @@
+package com.krakedev.aliens.test;
+
+import com.krakedev.aliens.Alien;
+
+public class TestConstructores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Alien alien1 = new Alien(10, "Verde");
+		Alien alien2 = new Alien(35, "Azul");
+		
+		System.out.println("---------ALIEN 1-----------");
+
+		alien1.imprimir();
+
+		System.out.println("---------ALIEN 2-----------");
+
+		alien2.imprimir();
+	}
+
+}
